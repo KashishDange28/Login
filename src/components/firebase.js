96151db4de278a044da20164c3f8b4d04a98d1c4
Kashish Dange <kashishdange@gmail.com>
2025-03-19 23:09:16 +0530

    Final

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -7,17 +7,16 @@ import {getFirestore} from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDrB9FupDgjjVk4_guLC24ydG3retE3bME",
-  authDomain: "login-auth-4736e.firebaseapp.com",
-  projectId: "login-auth-4736e",
-  storageBucket: "login-auth-4736e.appspot.com",
-  messagingSenderId: "10562914305",
-  appId: "1:10562914305:web:2cff37be4fa9ccf0a29800"
+  apiKey: "AIzaSyAUhx44FpUy_VNssbKnIxo05QBF3kAtrFg",
+  authDomain: "login-fe58f.firebaseapp.com",
+  projectId: "login-fe58f",
+  storageBucket: "login-fe58f.firebasestorage.app",
+  messagingSenderId: "390092052069",
+  appId: "1:390092052069:web:340e64a84f0545b758588c"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
 export const auth=getAuth();
 export const db=getFirestore(app);
 export default app;
